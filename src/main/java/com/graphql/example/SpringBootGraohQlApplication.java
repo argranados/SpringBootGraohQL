8bf1f0e1fcb495e361af925d8db7746bf90f36d3
Ciberaccion <argranados@yahoo.com>
2025-05-27 17:28:40 -0600

    first commit

---
+++ src/main/java/com/graphql/example/SpringBootGraohQlApplication.java
@@ -0,0 +1,13 @@
+package com.graphql.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraohQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraohQlApplication.class, args);
+	}
+
+}
